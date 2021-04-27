eedf7a9e1dc6b49e654ea70b68f5da1786187364
jannik <giese.jannik@web.de>
2021-04-27 19:26:42 +0200

    add arerts with 'if'

--- new.js
+++ new.js
@@ -4,5 +4,14 @@ const taskNameInput = document.querySelector(".form__input");
 formElement.onsubmit = function (event) {
   event.preventDefault();
   const radioGroupItem = document.querySelector(".radio-group__input:checked");
+  if (!taskNameInput.value) {
+    alert("enter Task");
+    return;
+  }
+  if (radioGroupItem == null) {
+    alert("Selct a Date");
+    return;
+  }
+
   console.log(taskNameInput.value, radioGroupItem.value);
 };
