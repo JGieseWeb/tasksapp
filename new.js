b140b172fe5d9fc10ac747eff0775bdbbf5228ce
jannik <giese.jannik@web.de>
2021-04-29 13:46:09 +0200

    add goToPage

--- new.js
+++ new.js
@@ -17,10 +17,15 @@ formElement.onsubmit = function (event) {
     name: taskNameInput.value,
     date: radioGroupItem.value,
   };
+
   const taskList = JSON.parse(localStorage.getItem("taskList"));
 
   taskList.push(task);
   console.log(taskList);
 
   localStorage.setItem("taskList", JSON.stringify(taskList));
+  function goToPage(href) {
+    location.href = href;
+  }
+  goToPage("/dashbord.html");
 };
