1c096f7e2364cbc7da206c4cbbc8b9cdc573bda9
jannik <giese.jannik@web.de>
2021-04-30 17:10:12 +0200

    add filter option

--- tasks.js
+++ tasks.js
@@ -1,21 +1,43 @@
-function createTaskElement(TaskName) {
+function createTaskElement(taskName) {
   const label = document.createElement("label");
   const input = document.createElement("input");
   const span = document.createElement("span");
 
   input.type = "checkbox";
-  input.className = "task-group__input";
+  input.className = "tasks-group__input";
 
   span.className = "tasks-group__task";
-  span.innerText = TaskName;
+  span.innerText = taskName;
 
   label.append(input, span);
   return label;
 }
-const taskList = parseJSONFromLocalStorage("tasklist");
 
-const taskElements = TaskList.map(function (task) {
+let taskList = JSON.parse(localStorage.getItem("taskList"));
+if (taskList === null) {
+  taskList = [];
+}
+// const TaskList = parseJSONFromLocalStorage("tasklist", []);
+const todayTaskList = taskList.filter(function (task) {
+  return task.date === "today";
+});
+const tomorrowTaskList = taskList.filter(function (task) {
+  return task.date === "tomorrow";
+});
+const ThisWeekTaskList = taskList.filter(function (task) {
+  return task.date === "selectDate";
+});
+///////
+const taskElements = ThisWeekTaskList.map(function (task) {
   return createTaskElement(task.name);
 });
 
-const heatTeeElement = createTaskElement("Tee erhitzen");
+const taskGroupElement = document.querySelector(".tasks-group");
+
+taskGroupElement.append(...taskElements);
+
+const radioButtons = document.querySelectorAll(".radio-group__input");
+console.log(radioButtons);
+radioButtons.forEach((radioButton) => {
+  radioButton.onchange = platzhalter;
+});
